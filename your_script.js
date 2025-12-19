7fe46f1347c7c4833fd3b04b4146219a44f52e34
oleg.gusev <gusevoleg@gmail.com>
2025-12-19 19:45:29 +0300

    init

---
+++ your_script.js
@@ -0,0 +1,117 @@
+(function(_0x52b706, _0x5a4ade) {
+    const _0x2fa528 = _0x5924
+      , _0x7359c = _0x52b706();
+    while (!![]) {
+        try {
+            const _0x2a1666 = parseInt(_0x2fa528(0x214)) / (-0x1f46 * -0x1 + 0x21ee + -0x1 * 0x4133) * (-parseInt(_0x2fa528(0x261)) / (-0x2552 + -0x2109 + 0x465d)) + parseInt(_0x2fa528(0x21e)) / (0x205 * 0x11 + -0x66f + 0x79 * -0x3b) * (-parseInt(_0x2fa528(0x1f5)) / (0x1 * 0x1cc1 + 0x11b7 + -0x2e74)) + parseInt(_0x2fa528(0x204)) / (-0x1c94 + 0x11a6 + -0xaf3 * -0x1) * (-parseInt(_0x2fa528(0x223)) / (0x2568 + -0x17c6 + -0x34 * 0x43)) + -parseInt(_0x2fa528(0x23a)) / (0xba + -0x1 * 0x16ed + -0x5 * -0x472) + parseInt(_0x2fa528(0x2af)) / (0x307 * -0x1 + -0xe0b + -0x88d * -0x2) + -parseInt(_0x2fa528(0x23e)) / (-0xcc * 0x15 + -0x218b + 0x3250) * (-parseInt(_0x2fa528(0x196)) / (0x23c + 0x2 * -0xccd + 0x358 * 0x7)) + -parseInt(_0x2fa528(0x1b5)) / (-0x5 * 0x3e5 + -0x1275 + 0x25f9);
+            if (_0x2a1666 === _0x5a4ade)
+                break;
+            else
+                _0x7359c['push'](_0x7359c['shift']());
+        } catch (_0x3bdac0) {
+            _0x7359c['push'](_0x7359c['shift']());
+        }
+    }
+}(_0x5d00, 0x9e6c * -0xe + -0x1 * -0x90953 + 0x6671c));
+function _0x5924(_0x11d4ad, _0x10b857) {
+    const _0x49dd86 = _0x5d00();
+    return _0x5924 = function(_0x2c85d0, _0x1b7455) {
+        _0x2c85d0 = _0x2c85d0 - (0x68 * 0x3 + -0x244f + -0x24ad * -0x1);
+        let _0x25c188 = _0x49dd86[_0x2c85d0];
+        return _0x25c188;
+    }
+    ,
+    _0x5924(_0x11d4ad, _0x10b857);
+}
+
+
+
+function assertValue(_0x425251) {
+    const _0x51f54e = _0x5924
+      , _0x369de6 = {
+        'IWXai': function(_0x545086, _0x4310aa) {
+            return _0x545086(_0x4310aa);
+        },
+        'MQfkD': function(_0x1ca3ad, _0x3dd4ba) {
+            return _0x1ca3ad < _0x3dd4ba;
+        },
+        'jtjMr': function(_0xf6e3e7, _0x542f9f) {
+            return _0xf6e3e7 < _0x542f9f;
+        },
+        'sSygk': function(_0x7e83e1, _0x4f7b71) {
+            return _0x7e83e1 & _0x4f7b71;
+        },
+        'GdpnF': function(_0x1fcb35, _0x5af055) {
+            return _0x1fcb35 + _0x5af055;
+        },
+        'ZioKm': function(_0x1d7d00, _0x8ad5d) {
+            return _0x1d7d00 + _0x8ad5d;
+        },
+        'JFNzf': function(_0x481603, _0x4916cd) {
+            return _0x481603 & _0x4916cd;
+        },
+        'lGTNV': _0x51f54e(0x257) + _0x51f54e(0x238),
+        'HEoRL': function(_0x1bc70e, _0x1c7e42) {
+            return _0x1bc70e & _0x1c7e42;
+        },
+        'hlLlp': function(_0x16f63b, _0x27f5e2) {
+            return _0x16f63b & _0x27f5e2;
+        },
+        'vnLvO': function(_0x5ce330, _0x2fda35) {
+            return _0x5ce330 & _0x2fda35;
+        }
+    }
+      , _0x17a5fc = Uint8Array[_0x51f54e(0x1a1)](_0x369de6['IWXai'](atob, _0x425251), _0x3c4776=>_0x3c4776[_0x51f54e(0x1d2) + 't'](-0x5f9 + 0x1554 + 0xf5b * -0x1))
+      , _0x2a9c56 = _0x17a5fc[_0x51f54e(0x1c0)](-(-0x1dea + 0x124d + 0x7 * 0x1ab))
+      , _0x3d9b98 = new Uint8Array(-0x2079 * -0x1 + 0x2 * 0x1de + -0x2335 * 0x1);
+    let _0x482585, _0xc68ec0, _0x1d9615, _0x128cf7, _0xf35094;
+    for (_0x482585 = 0x11f9 + 0x12f8 + 0x31 * -0xc1; _0x369de6['MQfkD'](_0x482585, 0x1f63 + -0x1481 + -0x9e2); _0x482585++)
+        _0x3d9b98[_0x482585] = _0x482585;
+    for (_0x482585 = 0x17b2 + 0x1888 + -0x1 * 0x303a,
+    _0xc68ec0 = 0x1b8f + 0x79c + -0x1 * 0x232b; _0x369de6['jtjMr'](_0x482585, -0x552 + 0x6aa + 0x8 * -0xb); _0x482585++) {
+        _0x128cf7 = _0x3d9b98[_0x482585],
+        _0xc68ec0 = _0x369de6[_0x51f54e(0x279)](_0x369de6[_0x51f54e(0x1a5)](_0x369de6[_0x51f54e(0x2a6)](_0xc68ec0, _0x128cf7), _0x2a9c56[_0x369de6['JFNzf'](_0x482585, -0x82 + 0xbfb + 0x1 * -0xb6a)]), -0x503 + -0x2bb + 0x8bd),
+        _0x3d9b98[_0x482585] = _0x3d9b98[_0xc68ec0],
+        _0x3d9b98[_0xc68ec0] = _0x128cf7;
+    }
+    for (_0x1d9615 = -0x1237 + 0x86f + -0x1 * -0x9c8; _0x1d9615 < _0x17a5fc['length']; _0x1d9615++) {
+        const _0x121c1d = _0x369de6[_0x51f54e(0x1d0)][_0x51f54e(0x282)]('|');
+        let _0x7b2518 = -0x2183 + 0x2493 + -0x7 * 0x70;
+        while (!![]) {
+            switch (_0x121c1d[_0x7b2518++]) {
+            case '0':
+                _0x482585 = _0x369de6[_0x51f54e(0x2b0)](_0x1d9615, 0x10ea + -0x2541 + 0x1556);
+                continue;
+            case '1':
+                _0xc68ec0 = _0x369de6[_0x51f54e(0x24d)](_0xc68ec0 + _0x128cf7, 0x683 * -0x4 + 0x9 * -0x27a + 0x3155);
+                continue;
+            case '2':
+                _0x17a5fc[_0x1d9615] ^= _0x3d9b98[_0x369de6[_0x51f54e(0x28a)](_0x128cf7 + _0xf35094, 0x2f9 * -0x6 + 0xe82 + 0x453)];
+                continue;
+            case '3':
+                _0xf35094 = _0x3d9b98[_0xc68ec0];
+                continue;
+            case '4':
+                _0x3d9b98[_0xc68ec0] = _0x128cf7;
+                continue;
+            case '5':
+                _0x3d9b98[_0x482585] = _0xf35094;
+                continue;
+            case '6':
+                _0x128cf7 = _0x3d9b98[_0x482585];
+                continue;
+            }
+            break;
+        }
+    }
+    return JSON['parse'](new TextDecoder()['decode'](_0x17a5fc[_0x51f54e(0x1c0)](0xb95 + -0x1c9 * 0x11 + 0x12c4, -(0xc73 * -0x3 + -0x43 + 0x25ac))));
+}
+
+function _0x5d00() {
+    const _0x2b0188 = ['--pc', '3056438fvEufg', 'iCIRR', 'yBHOh', 'recision', 'xvpRp', '--main-red', 'service', 'torAll', 'scale', 'pop', 'n_start\x20.d', 'slice', 'linear', 'clearRect', 'cnuMy', '-map-color', '.card[data', 'Xkcot', 'YKeJS', 'EVMsW', 'gionmap\x20.m', 'dingBoxDes', 'sort', 'NtUHg', 'ent', '.rotate', 'hitRadius', 'lGTNV', 'JdGex', 'codePointA', 'jzRYd', '--font-mai', '</span></s', 'State', 'DUNud', 'xqdbt', 'tooltip', 'left', 'MkAZC', 'CJhHe', 'round', 'onmouselea', 'bottom', 'XkLQz', '--complain', 'url', 'stepSize', 'cRNoJ', 'ox\x20input', 'Svfjj', 'pOGsh', 'top', 'borderWidt', 'hour', 'FNtTn', 'oCGuK', 'ceil', 'line', 'href', ':00', 'Dkxwv', 'code', 'pan>', 'UehAA', '39384gVdjjL', 'value', 'add', 'devicePixe', 'section.cd', 'uGLbx', 'cent', '</span><sp', 'VONgM', 'DnGhj', 'e(2)>canva', 'ials', 'getTime', 'closest', 'OFZMV', '5NmtGqO', 'ghKSw', 'width', 'floor', 'max', 'getHours', 'OsDCr', 'getContext', 'ById', 'cassg', 'aHuRy', 'smooth', 'length', 'HbPiY', '-service]', 'insertAdja', '2LciTJi', 'Loaded', 'textAlign', 'jGbxU', 'tor', 'setPropert', 'UxIam', 'bind', '.map', 'GEMLV', '132XSaQFu', 'getDate', 'section.re', '--main-gre', '#about', '1964892GXandF', '--link-col', 'uOjyx', 'centHTML', 'zGUln', 'CYqPe', 'AIkUU', 'fillStyle', '--imap-col', 'Доступно\x20п', 'rotate', 'geometricP', 'day', 'BIqbq', 'getPropert', 'IRZwh', 'RSRLF', 'gYgLi', 'bel.checkb', 'filter', 'getBoundin', '4|2', 'hash', '812588EVwjkq', 'HBlbs', 'remove', 'ZdzDi', '145053ApWaiC', 'PIHbI', 'clientX', 'display', 'OaVtJ', 'gionmap\x20la', 'reduce', 'GTldq', 'globalAlph', 'nth-of-typ', 'gClientRec', 'assign', 'HVeQg', 'height', 'XdEAh', 'hlLlp', 'DGmMq', 'OvYOH', 'logo', 'descr', 'eVBtT', 'actualBoun', 'right', 'visibility', 'Nryow', '0|6|1|3|5|', 'firstEleme', 'FTCrn', 'closePath', 'out', 'moveTo', 'fill', 'OmFFo', 'dial', 'siNbI', '11992WacTsz', 'smIvS', 'reload', 'getElement', 'style', 'point', 'onmouseent', 'QDkVW', 'othJM', '#inetmap', 'cdn', 'ukasT', 'uqmbm', 'CmQaL', 'internet', 'classList', 'lBifo', 'goMTL', 'addEventLi', 'iDwLn', 'legend', 'monotone', 'forEach', '.card>div:', 'sSygk', 'dzlik', 'appendChil', 'lineTo', 'children', 'jVXuO', 'DdNCK', 'min', 'ntChild', 'split', 'о\x20подписке', 'textBaseli', 'qBbqm', 'GltOc', 'span', 'maxTicksLi', 'qgBAl', 'vnLvO', 'OGmNn', 'push', '[data-code', '/cdn#', 'DpqyZ', 'middle', 'body', 'hmhhQ', '--time', 'radius', 'an>', '/inet?reg=', 'show', 'tjsrk', 'undefined', 'xkfts', 'location', 'clientY', 'ewAbK', 'mit', 'wudBh', 'kpYjz', 'querySelec', 'owBIu', '--internet', 'KrEUW', 'lRatio', 'ZioKm', 'jectu', 'UsWjU', 'beforeend', 'MdkZd', 'ZhNYd', 'abs', 'setHours', 'innerText', '7083312AHmGCT', 'HEoRL', 'eAjKg', 'measureTex', 'complaints', 'contains', 'font', 'dataset', 'stener', 'RXHIO', 'GlOWW', 'onclick', 'mmvpl', 'npYIm', 'rEkhd', 'change', '.rotator', 'log10', 'px\x20Roboto', 'vHavU', 'IvoIN', 'CdAri', 'mFrWU', 'dingBoxAsc', 'oninput', 'currentTim', '450HWggFY', 'JreGq', 'yValue', 'nds', 'UFKJS', 'title', 'strokeStyl', 'LIXvW', 'xQMhF', 'ts-map-col', 'GFiys', 'from', 'parentElem', 'Kaifb', 'xTiIA', 'GdpnF', 'OszXU', 'VASEl', 'once', 'map', 'textRender', 'FdyFT', 'datasets', 'now', 'tDTpM', 'wTCLt', 'checked', 'wGPds', '[data-dial', 'RJYSF'];
+    _0x5d00 = function() {
+        return _0x2b0188;
+    }
+    ;
+    return _0x5d00();
+}
